dd4dd0f87ddf9079748a76d0fe196194fed6f4a4
Kelvin Tan <kelvin.tyb@gmail.com>
2016-10-10 14:42:18 +0800

    added decelerate and stop functions

--- car.js
+++ car.js
@@ -4,12 +4,27 @@ var Car = function(color, convertible) {
   this.convertible = convertible;
 }
 
-Car.prototype.accelerate = function (num){
-this.speed += num
+Car.prototype.accelerate = function(num) {
+  this.speed += num
 }
 
-Car.prototype.setColor= function (color){
+Car.prototype.setColor = function(color) {
   this.color = newColor
+}
+
+Car.prototype.decelerate = function(rate) {
+  var oldSpeed = this.speed;
+  if (this.speed >= rate) {
+    this.speed = this.speed - rate;
+  } else {
+    this.speed = 0;
+  }
+  return "Old Speed: " + oldSpeed + "\nnewSpeed: " + this.speed;
+}
+
+Car.prototype.stop = function() {
+  this.speed = 0;
+  return 'Car has stopped now and current speed is: ' + this.speed;
 }
 
 module.exports = Car;
